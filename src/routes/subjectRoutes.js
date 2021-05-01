fd5564b9be703f90627d0e254a9d537c0c3c2090
Pham Toan <Pham Toan@PHAMTOAN>
2021-05-01 17:06:28 +0700

    fix authen update and delete subject

--- src/routes/subjectRoutes.js
+++ src/routes/subjectRoutes.js
@@ -1,20 +1,19 @@
-import { protect, admin } from "../middleware/authMiddleware.js";
-import express from "express";
+import { protect, admin } from '../middleware/authMiddleware.js'
+import express from 'express'
 import {
   getSubjects,
   getSubjectById,
   createSubject,
   updateSubject,
   deleteSubject,
-} from "../controllers/subjectController.js";
+} from '../controllers/subjectController.js'
 
-const router = express.Router();
+const router = express.Router()
 
+router.route('/', admin).get(getSubjects).post(createSubject)
 router
-  .route("/", admin)
-  .get(getSubjects)
-  .post(createSubject)
-  .put(updateSubject)
-  .delete(deleteSubject);
-router.route("/:id", admin).get(getSubjectById);
-export default router;
+  .route('/:id')
+  .get(getSubjectById)
+  .delete(protect, admin, deleteSubject)
+  .put(protect, admin, updateSubject)
+export default router
