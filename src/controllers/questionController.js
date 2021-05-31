49eb6efb6f0136b6bb21cd4332564698fbc6517a
zvici <nhatranthanh113@gmail.com>
2021-05-31 21:20:26 +0700

    update and delete question

--- src/controllers/questionController.js
+++ src/controllers/questionController.js
@@ -1,7 +1,7 @@
 import asyncHandler from "express-async-handler";
 import Question from "../models/question.model.js";
-import User from '../models/user.model.js'
-import Chapter from '../models/chapter.model.js'
+import User from "../models/user.model.js";
+import Chapter from "../models/chapter.model.js";
 
 // @desc   Fetch all questions
 // @route  GET /api/questions
@@ -20,29 +20,29 @@ const getQuestions = asyncHandler(async (req, res) => {
 // @route  GET /api/questions/:id
 // @access Public
 const getQuestionById = asyncHandler(async (req, res) => {
-    const question = await Question.findById(req.params.id);
-    if (question) {
-      res.send({
-        code: 0,
-        msg: "success",
-        message: `Info Question`,
-        data: chapter,
-      });
-    } else {
-      res.status(404);
-      throw new Error("Chapter not found");
-    }
+  const question = await Question.findById(req.params.id);
+  if (question) {
+    res.send({
+      code: 0,
+      msg: "success",
+      message: `Info Question`,
+      data: chapter,
+    });
+  } else {
+    res.status(404);
+    throw new Error("Chapter not found");
+  }
 });
 
 // @desc   Create one question
 // @route  Post /api/questions/
 // @access Public
 const createQuestions = asyncHandler(async (req, res) => {
-  const { title, chapter, result, answers, user, level } = req.body
+  const { title, chapter, result, answers, user, level } = req.body;
   if (title && chapter && result && user && level) {
-    let checkUser = await User.findById(user)
+    let checkUser = await User.findById(user);
     if (checkUser) {
-      let checkChapter = await Chapter.findById(chapter)
+      let checkChapter = await Chapter.findById(chapter);
       if (checkChapter) {
         let question = new Question({
           title: title,
@@ -51,63 +51,129 @@ const createQuestions = asyncHandler(async (req, res) => {
           level: level,
           chapter: chapter,
           user: user,
-        })
-        let newQuestion = await question.save()
+        });
+        let newQuestion = await question.save();
         res.send({
           code: 0,
-          msg: 'success',
-          message: 'Successfully created question',
+          msg: "success",
+          message: "Successfully created question",
           data: newQuestion,
-        })
+        });
       } else {
-        res.status(404)
-        throw new Error('Chapter not found')
+        res.status(404);
+        throw new Error("Chapter not found");
       }
     } else {
-      res.status(404)
-      throw new Error('User not found')
+      res.status(404);
+      throw new Error("User not found");
     }
   } else {
-    res.status(404)
-    throw new Error('Failure to create question')
+    res.status(404);
+    throw new Error("Failure to create question");
   }
-})
+});
 
 // @desc   Fetch all questions by chapter
 // @route  GET /api/questions/chapter/:id
 // @access Public
 const getQuestionsByChapter = asyncHandler(async (req, res) => {
-  const pageSize = 8
-  const page = Number(req.params.pageNumber) ? Number(req.params.pageNumber) : 1
-  const lvl = Number(req.params.level) ? Number(req.params.level) : {}
+  const pageSize = 8;
+  const page = Number(req.params.pageNumber)
+    ? Number(req.params.pageNumber)
+    : 1;
+  const lvl = Number(req.params.level) ? Number(req.params.level) : {};
 
   const key = req.query.keyword
     ? {
         title: {
           $regex: req.query.keyword,
-          $options: 'i',
+          $options: "i",
         },
       }
-    : {}
-  const count = await Question.countDocuments({ subject: req.params.id, level: lvl, ...key })
-  const questions = await Question.find({ subject: req.params.id, level: lvl, ...key })
+    : {};
+  const count = await Question.countDocuments({
+    subject: req.params.id,
+    level: lvl,
+    ...key,
+  });
+  const questions = await Question.find({
+    subject: req.params.id,
+    level: lvl,
+    ...key,
+  })
     .populate({
-      path: 'user',
+      path: "user",
     })
     .limit(pageSize)
-    .skip(pageSize * (page - 1))
+    .skip(pageSize * (page - 1));
   if (questions) {
     res.send({
       code: 0,
-      msg: 'success',
+      msg: "success",
       data: { questions, page, pages: Math.ceil(count / pageSize) },
-    })
+    });
   } else {
-    res.status(404)
-    throw new Error('Question not found')
+    res.status(404);
+    throw new Error("Question not found");
   }
-})
+});
+
+// @desc   Update questions by id
+// @route  PUT /api/questions/
+// @access Public
+
+const updateQuestionsById = asyncHandler(async (req, res) => {
+  const { id } = req.body;
+  if (id) {
+    let checkQuestion = await Question.findById(id);
+    if (checkQuestion) {
+      Question.updateOne({ _id: id }, { $set: req.body })
+        .exec()
+        .then(() => {
+          res.status(200).json({
+            code: 0,
+            msg: "success",
+            data: req.body,
+          });
+        })
+        .catch((err) => {
+          res.status(500).json({
+            code: 1,
+            msg: "Server error. Please try again.",
+          });
+        });
+    } else {
+      res.status(404);
+      throw new Error("Question not found");
+    }
+  } else {
+    res.status(404);
+    throw new Error("Id not found");
+  }
+});
+// @desc   Delete questions by id
+// @route  DELETE /api/questions/
+// @access Public
+const deleteQuestionsById = asyncHandler(async (req, res) => {
+  const delQuestion = await Question.findById(req.body.id);
+  if (delQuestion) {
+    await delQuestion.remove();
+    res.send({
+      code: 0,
+      msg: "success",
+      message: "Question Removed",
+    });
+  } else {
+    res.status(404);
+    throw new Error("Question not found");
+  }
+});
 
 export {
-  getQuestions,getQuestionById, createQuestions, getQuestionsByChapter
+  getQuestions,
+  getQuestionById,
+  createQuestions,
+  getQuestionsByChapter,
+  updateQuestionsById,
+  deleteQuestionsById,
 };
--- src/routes/questionRoutes.js
+++ src/routes/questionRoutes.js
@@ -1,11 +1,21 @@
-import { protect, admin } from '../middleware/authMiddleware.js'
-import express from 'express'
-import { createQuestions, getQuestions, getQuestionsByChapter } from '../controllers/questionController.js'
+import { protect, admin } from "../middleware/authMiddleware.js";
+import express from "express";
+import {
+  createQuestions,
+  getQuestions,
+  getQuestionsByChapter,
+  updateQuestionsById,
+  deleteQuestionsById,
+} from "../controllers/questionController.js";
 
-const router = express.Router()
+const router = express.Router();
 
-router.route('/').post(protect, admin, createQuestions).get(getQuestions)
-router.route('/chapter/:id/').get(getQuestionsByChapter)
+router
+  .route("/")
+  .post(protect, admin, createQuestions)
+  .get(protect, admin, getQuestions)
+  .put(protect, admin, updateQuestionsById)
+  .delete(protect, admin, deleteQuestionsById);
+router.route("/chapter/:id/").get(getQuestionsByChapter);
 
-
-export default router
+export default router;
